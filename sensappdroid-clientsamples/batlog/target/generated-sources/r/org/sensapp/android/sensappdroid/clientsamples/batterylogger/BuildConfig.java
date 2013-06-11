e5c46d3455cb5e6b4b4719cc0a79e440b4ee1cfc
brice-morin <brice.morin1@gmail.com>
2013-06-11 17:01:53 +0200

    remove batlog

--- sensappdroid-clientsamples/batlog/target/generated-sources/r/org/sensapp/android/sensappdroid/clientsamples/batterylogger/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package org.sensapp.android.sensappdroid.clientsamples.batterylogger;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
