75b7abd54a700640025442c59549dd3266abbc95
Jonathan Nain <jonathan.nain@gmail.com>
2013-06-27 16:00:56 +0200

    Add Jarsigner and Alignment

--- sensappdroid-clientsamples/sensorlog/target/generated-sources/r/org/sensapp/android/sensappdroid/clientsamples/sensorlogger/BuildConfig.java
+++ sensappdroid-clientsamples/sensorlog/target/generated-sources/r/org/sensapp/android/sensappdroid/clientsamples/sensorlogger/BuildConfig.java
@@ -1,5 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
 package org.sensapp.android.sensappdroid.clientsamples.sensorlogger;
 
 public final class BuildConfig {
-  public static final boolean DEBUG = true;
+    public final static boolean DEBUG = true;
 }
